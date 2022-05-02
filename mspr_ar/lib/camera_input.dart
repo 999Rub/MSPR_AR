6d73ad91386bc9cf1a4a4021c55d3d6dcc969d7f
999Rub <rubens.apprin73@gmail.com>
2022-05-02 18:09:58 +0200

    detection d'objet avec le model de la demo, dernière version de google ml kit

--- mspr_ar/lib/camera_input.dart
+++ mspr_ar/lib/camera_input.dart
@@ -6,6 +6,10 @@ import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:msprmlkit/ar_view.dart';
 import 'package:msprmlkit/main.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
+import 'dart:io' as io;
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 class CameraInput extends StatefulWidget {
   // List<CameraDescription> cameras;
@@ -19,6 +23,8 @@ class CameraInput extends StatefulWidget {
 
 class _CameraInputState extends State<CameraInput> {
   CameraController? controller;
+  bool detected = false;
+  Rect? rect;
 
   @override
   void initState() {
@@ -36,6 +42,21 @@ class _CameraInputState extends State<CameraInput> {
   void dispose() {
     controller?.dispose();
     super.dispose();
+  }
+
+  Future<String> _getModel(String assetPath) async {
+    if (io.Platform.isAndroid) {
+      return 'flutter_assets/$assetPath';
+    }
+    final path = '${(await getApplicationSupportDirectory()).path}/$assetPath';
+    await io.Directory(dirname(path)).create(recursive: true);
+    final file = io.File(path);
+    if (!await file.exists()) {
+      final byteData = await rootBundle.load(assetPath);
+      await file.writeAsBytes(byteData.buffer
+          .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+    }
+    return file.path;
   }
 
   @override
@@ -82,57 +103,107 @@ class _CameraInputState extends State<CameraInput> {
       home: ResponsiveSizer(builder: (context, orientation, screentype) {
         return CameraPreview(
           controller!,
-          child: Container(
-            height: 30,
-            width: 30,
-            child: CupertinoButton(
-              // Provide an onPressed callback.
-              onPressed: () async {
-                // Take the Picture in a try / catch block. If anything goes wrong,
-                // catch the error.
-                try {
-                  // Ensure that the camera is initialized.
-                  await widget.cameras;
+          child: !detected
+              ? Container(
+                  height: 30,
+                  width: 30,
+                  child: CupertinoButton(
+                    // Provide an onPressed callback.
+                    onPressed: () async {
+                      // Take the Picture in a try / catch block. If anything goes wrong,
+                      // catch the error.
+                      try {
+                        // Ensure that the camera is initialized.
+                        await widget.cameras;
 
-                  // Attempt to take a picture and then get the location
-                  // where the image file is saved.
-                  final image = await controller!.takePicture();
-                  print(image.path);
-                  final inputImage = InputImage.fromFilePath(image.path);
-                  const CustomLocalModel localModel = CustomLocalModel.asset;
-                  final imageLabeler = GoogleMlKit.vision.imageLabeler(
-                      CustomImageLabelerOptions(
-                          customModel: localModel,
-                          customModelPath: "model.tflite"));
+                        // Attempt to take a picture and then get the location
+                        // where the image file is saved.
+                        final image = await controller!.takePicture();
+                        print(image.path + ' PHOTO PRISE');
+                        final inputImage = InputImage.fromFilePath(image.path);
+                        final modelPath =
+                            await _getModel('assets/ml/object_labeler.tflite');
+                        final options = LocalObjectDetectorOptions(
+                            mode: DetectionMode.singleImage,
+                            modelPath: modelPath,
+                            classifyObjects: true,
+                            confidenceThreshold: 0.1,
+                            multipleObjects: true);
+                        // final LocalModel customModel = LocalModel("model.tflite");
+                        // final objectDetector = ObjectDetector(
+                        //     options: ObjectDetectorOptions(
+                        //         mode: DetectionMode.singleImage,
+                        //         classifyObjects: true,
+                        //         multipleObjects: false));
 
-                  final List<ImageLabel> labels =
-                      await imageLabeler.processImage(inputImage);
-                  //  LocalModel localModel = LocalModel(modelPath)
-                  for (ImageLabel label in labels) {
-                    final String text = label.label;
-                    final int index = label.index;
-                    final double confidence = label.confidence;
-                    print(label.label);
-                    print(label.confidence);
-                    if (text == "singe") {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: ((context) => HelloWorld())));
-                      break;
-                    }
-                    imageLabeler.close();
-                  }
-                } catch (e) {
-                  // If an error occurs, log the error to the console.
-                  print(e);
-                }
-              },
-              child: const Icon(Icons.camera_alt),
-            ),
-          ),
+                        final objectDetector = ObjectDetector(options: options);
+
+                        final List<DetectedObject> objects =
+                            await objectDetector.processImage(inputImage);
+
+                        if (objects.isEmpty) {
+                          print("AUCUN OBJET RETECTE ");
+                        } else {
+                          for (var object in objects) {
+                            print(object.boundingBox);
+                            print(object.labels.first.text);
+                          }
+                          // print(
+                          //     "${objects.first.boundingBox}, ${objects.first.labels.first.index},  ${objects.first.labels.first.text}");
+                          setState(() {
+                            detected = true;
+                            rect = objects.single.boundingBox;
+                          });
+                        }
+
+                        // print("PROCESS IMAGE TERMINE ${objects.length}");
+                        // for (DetectedObject detectedObject in objects) {
+
+                        //   final rect = detectedObject.boundingBox;
+                        //   final trackingId = detectedObject.trackingId;
+                        //   final label = detectedObject.labels;
+                        //   print(detectedObject.labels.length);
+                        //   for (Label label in detectedObject.labels) {
+                        //     print("AFFICHAGE DES INFORMATIONS : ");
+                        //     print('${label.index} ${label.confidence} ${label.text}');
+                        //   }
+                        //   print(rect);
+                        //   print(trackingId);
+                        // }
+
+                        objectDetector.close();
+                      } catch (e) {
+                        // If an error occurs, log the error to the console.
+                        print(e);
+                      }
+                    },
+                    child: const Icon(Icons.camera_alt),
+                  ),
+                )
+              : CustomPaint(
+                  painter: Box(rect: rect!),
+                ),
         );
       }),
     );
+  }
+}
+
+class Box extends CustomPainter {
+  Rect rect;
+  Box({required this.rect});
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawRect(
+      rect,
+      new Paint()
+        ..style = PaintingStyle.stroke
+        ..color = new Color(0xFF0099FF),
+    );
+  }
+
+  @override
+  bool shouldRepaint(Box oldDelegate) {
+    return false;
   }
 }
